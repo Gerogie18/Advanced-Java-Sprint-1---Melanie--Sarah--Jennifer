ca3cf384ecd91bd16b930f93c6273c2341b79339
Sarah Krisitan Perry <sarah.perry@keyin.com>
2025-02-16 17:59:27 -0330

    Added constructor, getters and setters, and returnAge method to Person class. Added test library and Person class test.

---
+++ tests/Resources/PersonTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+    Person person;
+
+    @BeforeEach
+    void setUp() {
+        person = new Person("Buddy", "Wassisname", "1981-01-31", "(709) 555-1234");
+    }
+
+    @Test
+    void returnAge() {
+        //this test will not work after Jan 2026...
+        int expected = 44;
+        assertEquals(expected, person.returnAge());
+    }
+
+}
