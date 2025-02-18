78315b6f851ed445a6f3ae7e0ed7c37d5395bac8
Gerogie18 <jennlyver@gmail.com>
2025-02-18 15:30:27 -0330

    Establish Constructor

---
+++ src/main/service/MedicationTrackingSystem.java
@@ -0,0 +1,24 @@
+package main.service;/*
+import main.model.Doctor;
+import main.model.Medication;
+import main.model.Patient;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+public class MedicationTrackingSystem {
+
+    private List<Patient> patients;
+    private List<Medication> medications;
+    private List<Doctor> doctors;
+
+    //constructor
+    public MedicationTrackingSystem() {
+        this.doctors = new ArrayList<>();
+        this.medications = new ArrayList<>();
+        this.patients = new ArrayList<>();
+    }
+
+}
+
