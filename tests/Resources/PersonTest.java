b57036a8b00012477c874b4b75ed2330908e015b
Gerogie18 <jennlyver@gmail.com>
2025-02-18 10:43:46 -0330

    remove test
    tests in separate folder.

--- tests/Resources/PersonTest.java
+++ tests/Resources/PersonTest.java
@@ -1,6 +1,5 @@
+import main.model.Person;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
     Person person;
