c14908705ffa2def6952e009b9f0cb443e0edc18
Gerogie18 <jennlyver@gmail.com>
2025-02-19 10:36:31 -0330

    add id logic
    tracks the largest id for doctor, patient, medication, and prescription

--- src/main/service/MedicationTrackingSystem.java
+++ src/main/service/MedicationTrackingSystem.java
@@ -1,11 +1,13 @@
-package main.service;/*
+package main.service;
 import main.model.Doctor;
+import main.model.Prescription;
 import main.model.Medication;
 import main.model.Patient;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
+import java.time.LocalDate;
 
 public class MedicationTrackingSystem {
 
@@ -13,17 +15,24 @@ public class MedicationTrackingSystem {
     private List<Medication> medications;
     private List<Doctor> doctors;
 
+    private int lastDoctorId;
+    private int lastPatientId;
+    private int lastMedicationId;
+    private int lastPrescriptionId;
+
     //constructor
     public MedicationTrackingSystem() {
         this.doctors = new ArrayList<>();
         this.medications = new ArrayList<>();
         this.patients = new ArrayList<>();
+        initializeIds();
     }
 
     public MedicationTrackingSystem(List<Patient> patients, List<Medication> medications, List<Doctor> doctors) {
         this.patients = patients;
         this.medications = medications;
         this.doctors = doctors;
+        initializeIds();
     }
 
 
@@ -49,6 +58,37 @@ public class MedicationTrackingSystem {
         return medications;
     }
     @Override
+    private int GenerateId (int id) {
+        int currentYear = LocalDate.now().getYear();
+        String currentMonthStr = String.format("%02d", LocalDate.now().getMonth().getValue());
+        int currentMonth = Integer.parseInt(currentMonthStr);
+        int  idFormat= (currentYear * 1000000)+ (currentMonth * 1000);
+        if ((id/1000) == (idFormat/1000)) {
+            return id += 1;
+        } else {
+            return id += 1;
+        }
+    }
+    private void initializeIds() {
+        this.lastDoctorId = doctors.stream()
+                .mapToInt(Doctor::getId)
+                .max()
+                .orElse(0);
+        this.lastPatientId = patients.stream()
+                .mapToInt(Patient::getId)
+                .max()
+                .orElse(0);
+        this.lastMedicationId = medications.stream()
+                .mapToInt(Medication::getId)
+                .max()
+                .orElse(0);
+        this.lastPrescriptionId = patients.stream()
+                .flatMap(patient -> patient.getPrescriptions().stream())
+                .mapToInt(Prescription::getId)
+                .max()
+                .orElse(0);
+    }
+
     public String toString() {
         return "MedicationTrackingSystem{" +
                 "patients=" + patients +
