ee0f9e96fbd25900215f0711baaf2ac6862aee6e
melanie <114537207+todayisthorsday@users.noreply.github.com>
2025-02-21 00:13:44 -0330

    class created for medication

--- src/main/model/Medication.java
+++ src/main/model/Medication.java
@@ -1,4 +1,23 @@
 package main.model;
 
+import java.time.LocalDate;
+
 public class Medication {
+    // Attributes
+    private int id;
+    private String name;
+    private String dose;
+    private int stockQuantity;
+    private LocalDate expiryDate;
+
+    // Constructor
+    public Medication(int id, String name, String dose, int stockQuantity, LocalDate expiryDate) {
+        this.id = id;
+        this.name = name;
+        this.dose = dose;
+        this.stockQuantity = stockQuantity;
+        this.expiryDate = expiryDate;
+    }
+
+ }
 }
